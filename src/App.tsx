d60ff3718e34fe900cef0891f522e138b9f06e56
mr_darcy <mr_darcy_1@mail.ru>
2022-12-02 04:36:53 +0500

    Count

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from "./App.module.css"
+import Button from "./components/Button";
 function App() {
+
+
+    const [number,setNumber] = useState(0)
+    const Plus = () => {
+        return number===5?setNumber(5): setNumber(number+1)
+    }
+    const Reload = () => {
+        return setNumber(0)
+    }
+
     return (
         <div className={classes.appContainer}>
-            Hello
+                <h1>{number}</h1>
+                <Button buttonName={'inc'} button={Plus}/>
+                <Button buttonName={'RESET'} button={Reload}/>
         </div>
     );
 }
