0a7a1a180ee70dc11cd49ab7520fbbdd9c387dde
mr_darcy <mr_darcy_1@mail.ru>
2022-12-03 20:56:17 +0500

    Count

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 import React, {useState} from 'react';
 import classes from "./App.module.css"
 import Button from "./components/Button";
+
 function App() {
 
 
-    const [number,setNumber] = useState(0)
+    const [number, setNumber] = useState(0)
     const Plus = () => {
-        return number===5?setNumber(5): setNumber(number+1)
+        const newValue = number+1
+        number<5 && setNumber(newValue)
+        console.log(newValue)
     }
     const Reload = () => {
-        return setNumber(0)
+        number===5&& setNumber(0)
     }
+
+    const classesNumber = number===5?classes.counter + ' ' + classes.red:classes.counter
 
     return (
         <div className={classes.appContainer}>
-            <div className={classes.counter}><h1 className={classes.number}>{number}</h1>
+            <div className={classesNumber}><h1 className={classes.number}>{number}</h1>
                 <div className={classes.buttons}>
-                    <Button buttonName={'inc'} button={Plus}/>
-                    <Button buttonName={'RESET'} button={Reload}/></div>
+                    <Button buttonName={'inc'} disabled={number===5} button={Plus}/>
+                    <Button buttonName={'RESET'} disabled={number<5} button={Reload}/>
+                </div>
             </div>
         </div>
     );
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 type ButtonPropsType = {
     buttonName:string,
     button:()=>void
+    disabled:boolean
 }
 
 const Button = (props:ButtonPropsType) => {
     return (
         <div>
-            <button onClick={props.button}>{props.buttonName}</button>
+            <button onClick={props.button} disabled={props.disabled}>{props.buttonName}</button>
         </div>
     );
 };
